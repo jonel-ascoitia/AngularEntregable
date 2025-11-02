d0109805e67678c640284261640f843a9b0d0f7a
jair-angel777 <1636763@senati.pe>
2025-11-02 14:39:36 -0500

    primera evidencia

--- src/app/user/user-add/user-add.component.ts
+++ src/app/user/user-add/user-add.component.ts
@@ -1,1 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserInterface } from '../user-list/UserInterface';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
 
+
+@Component({
+  selector: 'app-user-add',
+  standalone: true,
+  imports: [ ReactiveFormsModule ],
+  templateUrl: './user-add.component.html',
+  styleUrl: './user-add.component.css'
+})
+export class UserAddComponent {
+navigateTo(arg0: string) {
+throw new Error('Method not implemented.');
+}
+addNewUser() {
+throw new Error('Method not implemented.');
+}
+  formulario = new FormGroup({
+
+    id: new FormControl(''),
+    name: new FormControl(''),
+    lastname: new FormControl('')
+  });
+
+  newUser: UserInterface = {
+    id: 0,
+    name: "",
+    lastname: ""
+  }
+}
